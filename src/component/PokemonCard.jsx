47f68c0ac923c8eb8b4501dd373ccca41e2ed143
Hamidou Mamadou <midou88530@gmail.com>
2023-12-12 12:16:59 +0100

    Dynamique Pokemon

---
+++ src/component/PokemonCard.jsx
@@ -0,0 +1,23 @@
+const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+
+  function PokemonCard () {
+    const pokemon = pokemonList[0]
+    return <figure>
+
+        {pokemon.imgSrc ? (<img src={pokemon.imgSrc}/>) : (<p>???</p>)}
+        <div>{pokemon.name}</div>
+
+        
+    </figure>
+  }
+
+  export default PokemonCard;
